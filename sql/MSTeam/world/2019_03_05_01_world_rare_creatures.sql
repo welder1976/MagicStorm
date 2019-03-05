732c095c700c7cf2d06b68c43aa2fe2526369e0e
Kulyadev <al.kulya@angelic.com>
2019-03-05 20:59:05 +0300

    Sha of Anger pool

--- sql/MSTeam/world/2019_03_05_01_world_rare_creatures.sql
+++ sql/MSTeam/world/2019_03_05_01_world_rare_creatures.sql
@@ -1,8 +1,9 @@
-DELETE FROM pool_template WHERE entry IN (460, 461);
+DELETE FROM pool_template WHERE entry IN (460, 461, 571);
 INSERT INTO pool_template (entry, max_limit, description) VALUES
 (460, 1, "Bayne"),
-(461, 1, "Farmer Solliden");
-DELETE FROM pool_creature WHERE pool_entry IN (460, 461);
+(461, 1, "Farmer Solliden"),
+(571, 1, "Sha of Anger");
+DELETE FROM pool_creature WHERE pool_entry IN (460, 461, 571);
 INSERT INTO pool_creature (guid, pool_entry, chance, description) VALUES
 (131458, 460, 0, "Bayne (Spawn 1)"),
 (131459, 460, 0, "Bayne (Spawn 2)"),
@@ -11,4 +12,8 @@ INSERT INTO pool_creature (guid, pool_entry, chance, description) VALUES
 (52641, 460, 0, "Bayne (Spawn 5)"),
 (306498, 460, 0, "Bayne (Spawn 6)"),
 (17528, 461, 0, "Farmer Solliden (Spawn 1)"),
-(194009, 461, 0, "Farmer Solliden (Spawn 2)");
+(194009, 461, 0, "Farmer Solliden (Spawn 2)"),
+(10576396, 461, 0, "Sha of Anger (Spawn 1)"),
+(10576395, 461, 0, "Sha of Anger (Spawn 2)"),
+(10550815, 461, 0, "Sha of Anger (Spawn 3)"),
+(10550814, 461, 0, "Sha of Anger (Spawn 4)");
