4fb7a9af4c0e9d70c0c0c9ec5102f97fa27ad9a6
Kulyadev <al.kulya@angelic.com>
2019-03-20 15:16:17 +0300

    Fix Gwen Armstead bct

--- sql/MSTeam/world/2019_03_09_00_world_worgen_start_zone.sql
+++ sql/MSTeam/world/2019_03_09_00_world_worgen_start_zone.sql
@@ -742,6 +742,10 @@ INSERT INTO creature_text (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Langu
 (34913, 0, 3, "Push them back!", 14, 0, 20, 0, 0, 19620, "Prince Liam Greymane", 35169),
 (34913, 0, 4, "Take heart, men! We must protect our city!", 14, 0, 20, 0, 0, 19621, "Prince Liam Greymane", 35170);
 
+UPDATE creature_text SET BroadcastTextId = 36094 WHERE CreatureID = 35840 AND GroupID = 0 AND ID = 0;
+UPDATE creature_text SET BroadcastTextId = 36093 WHERE CreatureID = 35840 AND GroupID = 1 AND ID = 0;
+UPDATE creature_text SET BroadcastTextId = 36095 WHERE CreatureID = 35840 AND GroupID = 2 AND ID = 0;
+
 UPDATE creature_template SET AIName = "SmartAI" WHERE entry IN (35873, 35869, 44459, 44469);
 DELETE FROM `smart_scripts` WHERE `entryorguid` IN (35873, 35869, 44459, 44469) AND `source_type` = 0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
