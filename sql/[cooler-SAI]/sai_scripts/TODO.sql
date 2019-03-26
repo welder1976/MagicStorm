f2bffcbcddb32b80d89b98b450b9db92ef49fdc5
cooler-SAI <cooler19860212@yahoo.com>
2019-03-26 11:06:04 +0300

    DB/SAI: added TODO

---
+++ sql/[cooler-SAI]/sai_scripts/TODO.sql
@@ -0,0 +1,105 @@
+SmartAI Scripts TODO:
+
+WoD Zones:
+	
+-- Talador	
+
+-- Tanaan Jungle	
+
+WoD Dungeons:
+
+WoD Raids:
+
+http://www.wowhead.com/level-90-100-zones#zones:0+1+2+4
+
+
+Legion Zones:
+
+-- Stormheim	
+
+-- Highmountain	
+
+-- Val'sharah	
+
+-- Azsuna	
+
+-- Broken Shore	
+
+-- Suramar
+
+Legion Dungeons:
+
+-- Return to Karazhan
+
+-- Violet Hold
+
+Legion Raids:
+
+-- Antorus, the Burning Throne	
+
+-- The Nighthold	
+
+-- The Emerald Nightmare	
+
+-- Tomb of Sargeras	
+
+-- Trial of Valor
+
+http://www.wowhead.com/level-100-110-zones#zones:0+1+2+4
+
+\\ -- 8.1.x patch \\
+
+BfA Zones:
+
+-- Drustvar	
+
+-- Nazmir	
+
+-- Stormsong Valley	
+
+-- Tiragarde Sound	
+
+-- Tol Dagor	
+
+-- Tol Dagor	
+
+-- Tradewinds Market	
+
+-- Vol'dun	
+
+-- Zuldazar
+
+BfA Dungeons:
+
+-- Atal'Dazar	
+
+-- Siege of Boralus	
+
+-- Tol Dagor	
+
+-- Freehold	
+
+-- Kings' Rest	
+
+-- Temple of Sethraliss	
+
+-- Shrine of the Storm	
+
+-- The Underrot	
+
+-- Waycrest Manor
+
+BfA Raids:
+
+-- Crucible of Storms	
+
+-- Battle of Dazar'alor	
+
+-- Uldir
+
+
+https://www.wowhead.com/level-110-120-zones#zones:0+1+2+4
+
+
+
+
