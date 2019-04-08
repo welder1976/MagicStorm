5de50d0ef79adff4176cc56329423a996109b445
Kulyadev <al.kulya@angelic.com>
2019-04-08 22:56:21 +0300

    Clara Charles: Added event

--- sql/MSTeam/world/2019_02_10_00_world_some_fixes.sql
+++ sql/MSTeam/world/2019_02_10_00_world_some_fixes.sql
@@ -632,8 +632,55 @@ INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, 
 (178448, 179877, 5, 315, 515),
 (178448, 179876, 8, 340, 515);
 
+DELETE FROM creature WHERE guid = 302899 AND id = 583;
+UPDATE creature_template SET AIName = "SmartAI" WHERE entry IN (5917, 583);
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN (5917, 583) AND `source_type` = 0;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+(5917, 0, 0, 0, 34, 0, 100, 0, 2, 5, 0, 0, 12, 583, 4, 180000, 0, 0, 0, 8, 0, 0, 0, -9232.11, 342.473, 74.4399, 4.31658, "Clara Charles - On WP 5 Reached - Summon Creature 'Defias Ambusher'"),
+
+(583, 0, 0, 0, 4, 0, 30, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Defias Ambusher - On Aggro - Say Line 0 (No Repeat)"),
+(583, 0, 1, 0, 54, 0, 100, 1, 0, 0, 0, 0, 53, 0, 583, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, "Defias Ambusher - On Just Summoned - Start WP (No Repeat)"),
+(583, 0, 2, 0, 40, 0, 100, 0, 5, 583, 0, 0, 54, 7000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Defias Ambusher - On WP 5 Reached - WP Pause (7000 ms)"),
+(583, 0, 4, 0, 40, 0, 100, 0, 11, 583, 0, 0, 41, 2000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Defias Ambusher - On WP 11 Reached - Despawn (2000 ms)");
+
+UPDATE creature SET spawndist = 0, MovementType = 2 WHERE guid = 177192 AND id = 5917;
+DELETE FROM creature_template_addon WHERE entry = 5917;
+DELETE FROM creature_addon WHERE guid = 177192;
+INSERT INTo creature_addon (guid, path_id, bytes1, bytes2) VALUES
+(177192, 177192*10, 0, 4097);
+
+DELETE FROM waypoint_data WHERE id = 177192*10;
+INSERT INTO waypoint_data (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_type`, `action`, `action_chance`, `wpguid`) VALUES
+(177192*10, 1, -9266.12, 354.4, 76.6919, 2.2945, 60000, 0, 0, 100, 0),
+(177192*10, 2, -9265.77, 353.952, 76.692, 5.37954, 0, 0, 0, 100, 0),
+(177192*10, 3, -9259.54, 346.55, 75.7548, 5.44709, 0, 0, 0, 100, 0),
+(177192*10, 4, -9240.37, 343.05, 74.8524, 0.194343, 0, 0, 0, 100, 0),
+(177192*10, 5, -9225.87, 355.227, 73.4195, 0.68129, 0, 0, 0, 100, 0),
+(177192*10, 6, -9215.68, 363.51, 72.1801, 1.00959, 30000, 0, 0, 100, 0),
+(177192*10, 7, -9223.65, 358.136, 72.7038, 3.73649, 0, 0, 0, 100, 0),
+(177192*10, 8, -9251.11, 339.555, 75.0156, 3.73649, 0, 0, 0, 100, 0),
+(177192*10, 9, -9259.98, 347.165, 75.7808, 2.28743, 0, 0, 0, 100, 0),
+(177192*10, 10, -9265.62, 353.631, 76.6927, 2.28743, 0, 0, 0, 100, 0),
+(177192*10, 11, -9266.12, 354.4, 76.6919, 2.2945, 180000, 0, 0, 100, 0);
+
+DELETE FROM waypoints WHERE entry = 583;
+INSERT INTO waypoints (`entry`, `pointid`, `position_x`, `position_y`, `position_z`, `point_comment`) VALUES
+(583, 1, -9232.11, 342.473, 74.4399, 'Defias Ambusher'),
+(583, 2, -9227.82, 341.992, 73.9688, 'Defias Ambusher'),
+(583, 3, -9221.57, 347.109, 73.9924, 'Defias Ambusher'),
+(583, 4, -9216.57, 357.231, 72.8203, 'Defias Ambusher'),
+(583, 5, -9216.18, 362.686, 72.1863, 'Defias Ambusher'),
+(583, 6, -9219.08, 353.811, 73.1801, 'Defias Ambusher'),
+(583, 7, -9222.54, 341.161, 73.8594, 'Defias Ambusher'),
+(583, 8, -9224.02, 326.395, 73.0095, 'Defias Ambusher'),
+(583, 9, -9226.26, 316.182, 74.112, 'Defias Ambusher'),
+(583, 10, -9231.02, 306.332, 74.406, 'Defias Ambusher'),
+(583, 11, -9239.83, 305.172, 74.2633, 'Defias Ambusher');
+
+
+
+
 -- 177459 - WPs
--- 177192 - RP Event
 -- ID: 11328 - WPs + Emotes
 -- GUID: 179023 - OOC Texts
 
